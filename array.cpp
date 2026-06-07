73b972e8b10dacad9f65e5bdc8e9c7107657ed63
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2026-06-07 15:22:25 -0400

    Changed greeting
    Changed greeting and increment version

--- array.cpp
+++ array.cpp
@@ -9,7 +9,7 @@
 	#include <libgen.h>
 	#include <sys/utsname.h>
 #endif
-const double version = 2.30;
+const double version = 2.31;
 using namespace std;
 char *myname(char *nav) {
 	#if defined(__APPLE__) || defined(__linux__) || defined(__FreeBSD__) || defined(__EMSCRIPTEN__) || defined(__CYGWIN__) || defined(__wasi__) || defined(__NetBSD__)
@@ -25,7 +25,7 @@ char *myname(char *nav) {
 int main(int ac, char *av[]) {
 	cout<<myname(av[0])<<": Welcome to version "<<version<<"..."<<endl;
 	cout<<"Copyright 2026 Wilhelm Payne."<<endl;
-	cout<<"King Jellyfish loves us!"<<endl;
+	cout<<"King Jellyfish loves us and so does Queen!!"<<endl;
 #if defined(__APPLE__) || defined(__linux__) || defined(__FreeBSD__) || defined(__EMSCRIPTEN__) || defined(__CYGWIN__) || defined(__wasi__) || defined(__NetBSD__)
 	struct utsname un;
 	char nu[256];
